5de8ddc85bae27b3260f0f6115756fdd50b5ae52
noidoRG <noidorga@gmail.com>
2023-07-07 11:52:16 +0300

    Добавил механику стрельбы

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BulletController : MonoBehaviour
+{
+    public float lifeTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DeathDelay());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator DeathDelay()
+    {
+        yield return new WaitForSeconds(lifeTime);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rigidBody2D;
     public TextMeshProUGUI collectedText;
     public static int collectedAmount = 0;
+    public GameObject bulletPrefab;
+    public float bulletSpeed;
+    private float lastFire;
+    public float fireDelay;
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +25,29 @@ public class PlayerController : MonoBehaviour
     {
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        float shootHorizontal = Input.GetAxis("ShootHorizontal");
+        float shootVertical = Input.GetAxis("ShootVertical");
+
+        if ((shootHorizontal != 0 || shootVertical != 0) && Time.time > lastFire + fireDelay)
+        {
+            Shoot(shootHorizontal, shootVertical);
+            lastFire = Time.time;
+        }
+
         rigidBody2D.velocity = new Vector3(horizontal * speed, vertical * speed, 0);
         collectedText.text = "Items Collected: " + collectedAmount;
+    }
+
+    void Shoot(float x, float y)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+        bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
+        bullet.GetComponent<Rigidbody2D>().velocity = new Vector3(
+         (x < 0) ? Mathf.Floor(x) * bulletSpeed :Mathf.Ceil(x) * bulletSpeed,
+         (y < 0) ? Mathf.Floor(y) * bulletSpeed :Mathf.Ceil(y) * bulletSpeed,
+         0
+        );
+        if (bulletPrefab != null) { }
     }
 }
