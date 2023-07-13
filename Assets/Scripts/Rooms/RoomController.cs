52e20ca6ffb15a7b148c7b977eed2f3782e3950f
noidoRG <noidorga@gmail.com>
2023-07-13 10:34:18 +0300

    Сделал дефолтный вид комнаты, добавил коллайдеры на стены

---
+++ Assets/Scripts/Rooms/RoomController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RoomInfo
+{
+    public string name;
+    public int X;
+    public int Y;
+}
+
+public class RoomController : MonoBehaviour
+{
+    public static RoomController instance;
+    string currentWorldName = "The Junkyard";
+    RoomInfo currentLoadRoomData;
+    Queue<RoomInfo> loadRoomQueue = new Queue<RoomInfo>();
+    public List<Room> loadedRooms = new List<Room>();
+    bool isLoadingRoom = false;
+    
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public bool DoesRoomExist(int x, int y)
+    {
+        return loadedRooms.Find(item => item.X == x && item.Y  == y) != null;
+    }
+
+     // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
