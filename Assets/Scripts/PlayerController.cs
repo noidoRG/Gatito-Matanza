13e77511211b2f3498253663a5063f66bcdafd7d
noidoRG <noidorga@gmail.com>
2023-07-15 23:19:50 +0300

    Настроил анимацию ходьбы игрока

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,6 @@ using Unity.VisualScripting;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float speed;
     public TextMeshProUGUI collectedText;
     public static int collectedAmount = 0;
@@ -16,39 +15,47 @@ public class PlayerController : MonoBehaviour
     public float fireDelay;
 
     Rigidbody2D rigidBody2D;
+    Animator animator;
     Vector2 movementInput;
     Vector2 smoothedMovementInput;
     Vector2 movementInputSmoothedVelocity;
+    bool isWalking;
 
-
-    // Start is called before the first frame update
     void Awake()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
-
+        animator = GetComponent<Animator>();
     }
-
 
     private void FixedUpdate()
     {
         smoothedMovementInput = Vector2.SmoothDamp(smoothedMovementInput, movementInput, ref movementInputSmoothedVelocity, 0.1f);
         rigidBody2D.velocity = smoothedMovementInput * speed;
+
+        // Установка параметров анимации
+        animator.SetFloat("Horizontal", smoothedMovementInput.x);
+        animator.SetFloat("Vertical", smoothedMovementInput.y);
+        animator.SetBool("isWalking", isWalking);
     }
 
     void OnMove(InputValue inputValue)
     {
         movementInput = inputValue.Get<Vector2>();
 
+        if (movementInput != Vector2.zero)
+        {
+            isWalking = true;
+        }
+        else
+        {
+            isWalking = false;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
         fireDelay = GameController.FireRate;
         speed = GameController.MoveSpeed;
-
-        //float horizontal = Input.GetAxis("Horizontal");
-        //float vertical = Input.GetAxis("Vertical");
 
         float shootHorizontal = Input.GetAxis("ShootHorizontal");
         float shootVertical = Input.GetAxis("ShootVertical");
@@ -59,7 +66,6 @@ public class PlayerController : MonoBehaviour
             lastFire = Time.time;
         }
 
-        //rigidBody2D.velocity = new Vector3(horizontal * speed, vertical * speed, 0);
         collectedText.text = "Items Collected: " + collectedAmount;
     }
 
@@ -68,9 +74,9 @@ public class PlayerController : MonoBehaviour
         GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
         bullet.AddComponent<Rigidbody2D>().gravityScale = 0;
         bullet.GetComponent<Rigidbody2D>().velocity = new Vector3(
-         (x < 0) ? Mathf.Floor(x) * bulletSpeed :Mathf.Ceil(x) * bulletSpeed,
-         (y < 0) ? Mathf.Floor(y) * bulletSpeed :Mathf.Ceil(y) * bulletSpeed,
-         0
+            (x < 0) ? Mathf.Floor(x) * bulletSpeed : Mathf.Ceil(x) * bulletSpeed,
+            (y < 0) ? Mathf.Floor(y) * bulletSpeed : Mathf.Ceil(y) * bulletSpeed,
+            0
         );
         if (bulletPrefab != null) { }
     }
